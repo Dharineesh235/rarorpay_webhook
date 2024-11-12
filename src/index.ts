09347d9d8723b868a77ffeaf73a98f5a5a5a0a23
Dharineesh G G <dharineeshgg@Dharineeshs-MacBook-Air.local>
2024-11-12 16:23:13 +0530

    PORT not exposed

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { handleRazorpayEvent } from './controller/smsController';
 import cors from 'cors';
 
-const PORT = 5008;
+const PORT = process.env.PORT || 5008;
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -10,4 +10,4 @@ app.use(cors());
 app.get('/', (req, res)=>{console.log('/get'); res.send('/get')});
 app.post('/webhook', handleRazorpayEvent);
 
-app.listen(PORT, ()=>console.log('listening...'));
+app.listen(PORT, ()=>console.log('listening... '+PORT));
